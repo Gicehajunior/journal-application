6f4d2aa00d4ebfeb290dcd07309bd0268f245c10
Gicehajunior <gicehajunior76@gmail.com>
2025-03-20 13:22:58 +0300

    added iso 8601 date formatter func

--- config/utils.js
+++ config/utils.js
@@ -8,6 +8,17 @@ const utility = {
     ucfirst: (str) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     },
+    dateToISOformatter: (date) => {
+        if (!date) return "";
+        return date.toISOString().split('T')[0]; // Converts to 'YYYY-MM-DD'
+    },
+    dateToISO8601Formatter: (dateString) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
 }
 
 module.exports = utility;

006eacb7183c0d5177ef7b01ff482b7b964b1a64
Gicehajunior <gicehajunior76@gmail.com>
2025-03-20 14:55:18 +0300

    add nl2br helper function

--- config/utils.js
+++ config/utils.js
@@ -18,6 +18,9 @@ const utility = {
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
+    },
+    nl2br: (text) => {
+        return text.replace(/\n/g, '<br>');
     }
 }
 

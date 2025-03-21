29b7f83a4fb5d66589f051a79e4c98bb9165cc98
Gicehajunior <gicehajunior76@gmail.com>
2025-03-21 14:15:14 +0300

    add fontawesome resource pattern endpoints

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,8 @@ module.exports = (env, argv) => {
                     { from: './node_modules/jquery/dist/*.js', to: 'assets/jquery/js/[name][ext]' },
                     { from: './node_modules/datatables/media/images', to: 'assets/jquery/images' },
                     // ADD NEW RESOURCE TO BE COPIED HERE IF NEEDED.
+                    { from: './node_modules/@fortawesome/fontawesome-free/css/*.css', to: 'assets/fontawesome/css/[name][ext]' },
+                    { from: './node_modules/@fortawesome/fontawesome-free/js/*.js', to: 'assets/fontawesome/js/[name][ext]' }, 
                 ]
             })
         ],
